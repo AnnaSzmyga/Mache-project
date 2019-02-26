55ebfc441b64cb05d168e8c164f8e399b67ade9a
Anna Szmyga <anna.szmyga@o2.pl>
2019-02-26 18:40:11 +0100

    Add modal

--- js/script.js
+++ js/script.js
@@ -7,6 +7,31 @@ document.querySelector('.hamburger').addEventListener('click', function(){
 });
 
 
+// Changing text slides in 'quotes' section
+
+var slides = document.querySelectorAll('.quotes-text');
+function showPreviousSlide() {
+	for (var i = 1; i < slides.length; i++) {
+		if (slides[i].classList.contains('active')) {
+			slides[i].classList.remove('active');
+			slides[i - 1].classList.add('active');
+		}
+	}
+}
+function showNextSlide() {
+	for (var i = 0; i < (slides.length - 1); i++) {
+		if (slides[i].classList.contains('active')) {
+			slides[i].classList.remove('active');
+			slides[i + 1].classList.add('active');
+		}
+	}
+}
+
+document.querySelector('.btn-previous').addEventListener('click', showPreviousSlide);
+document.querySelector('.btn-next').addEventListener('click', showNextSlide);
+
+
+
 // Changing images decorations width whenever browser window is resizing
 
 function setDecorationsWidth(){
@@ -19,3 +44,23 @@ function setDecorationsWidth(){
 setDecorationsWidth();
 
 window.addEventListener("resize", setDecorationsWidth);
+
+
+// Modal
+
+function hideModal(){
+	document.querySelector('.overlay').classList.add('hide-modal');
+};
+document.querySelector('.close').addEventListener('click', hideModal);
+
+document.querySelector('.overlay').addEventListener('click', function(event) {
+  if (event.target === this) {
+    hideModal();
+ }
+});
+
+document.addEventListener('keyup', function(event) {
+  if (event.keyCode === 27) {
+    hideModal();
+  }
+});
